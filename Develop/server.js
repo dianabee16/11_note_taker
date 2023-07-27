22834c2efdf9a6c4b6dc19c039a4695ed8a9a673
dianabee <dianabee16@gmail.com>
2023-07-27 10:40:07 -0700

    added read route

--- Develop/server.js
+++ Develop/server.js
@@ -6,8 +6,26 @@ const app =  express()
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
-}))
+}));
 app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+});
+
+app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if(err) throw err;
+        let dbData = JSON.parse(data)
+        res.json(dbData)
+    })
+})
+
+
 app.listen(PORT, () => {
     console.log(`app.listening: ${PORT}`)
 })
